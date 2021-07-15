c848db9a1be7cea407ac4afe8b9f02a57a186934
Lorenz Hufe <hufe@kiew.kl.dfki.de>
2021-07-15 11:45:57 +0200

    fix setup.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-srun \
---container-image=/netscratch/hufe/slt_image.sqsh \
---container-mounts=/netscratch/$USER:/netscratch/$USER,/ds/text:/data,./slt:/workspace \
---gpus=1 \
---nodes=1 \
---ntasks=1 \
-python -m signjoey train configs/sign.yaml
+srun --container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh --container-mounts=/netscratch/hufe/slt:/netscratch,/ds/text:/data,slt:/workspace --gpus=1 --nodes=1 --ntasks=1 --mem=42G python -m signjoey train configs/sign.yaml
--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 mkdir -p "/netscratch/$USER/slt/models"
 
-echo "run \
---container-image=/netscratch/hufe/slt_image.sqsh \
+echo "srun \
+--container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh \
 --container-mounts=/netscratch/$USER/slt:/netscratch,/ds/text:/data,slt:/workspace \
--gpus=1 \
+--gpus=1 \
 --nodes=1 \
 --ntasks=1 \
 --mem=42G \
 python -m signjoey train configs/sign.yaml" > ./slt/run.sh
-
 
 ./slt/run.sh
