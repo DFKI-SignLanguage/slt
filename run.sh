395cb6c31bc24917b6c75442001ae2201fab562e
Lorenz Hufe <hufe@kiew.kl.dfki.de>
2022-05-19 09:48:28 +0200

    changed setup scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-srun --container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh --container-mounts=/netscratch/hufe/slt:/netscratch,/ds/text:/data,slt:/workspace --gpus=1 --nodes=1 --ntasks=1 --mem=42G python -m signjoey train configs/sign.yaml
+srun --container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh --container-mounts=/netscratch/software:/netscratch/software:ro,/netscratch/hufe/slt:/netscratch/slt,/ds/text:/data,/home/hufe/slt:/workspace --gpus=1 --mem=64G python -m signjoey train configs/sign.yaml
--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@ mkdir -p "/netscratch/$USER/slt/models"
 
 echo "srun \
 --container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh \
---container-mounts=/netscratch/$USER/slt:/netscratch,/ds/text:/data,slt:/workspace \
+--container-mounts=/netscratch/software:/netscratch/software:ro,/netscratch/$USER/slt:/netscratch/slt,/ds/text:/data,/home/$USER/slt:/workspace \
 --gpus=1 \
---nodes=1 \
---ntasks=1 \
---mem=42G \
+--mem=64G \
 python -m signjoey train configs/sign.yaml" > ./slt/run.sh
 
 ./slt/run.sh
